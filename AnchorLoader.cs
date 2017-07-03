602bdd0b5cde5df1ff8c43e3b613bd39056865ec
Eric Catlin <eric.catlin@indigoslate.com>
2017-07-03 11:15:46 -0700

    AchorLoader is a nice way to handle loading of a specific anchor for an obejct

---
+++ AnchorLoader.cs
@@ -0,0 +1,15 @@
+﻿using HoloToolkit.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AnchorLoader : MonoBehaviour {
+
+    public AnchorManager AnchorManager;
+
+    public void OnAnchorStoreReady()
+    {
+        AnchorManager.Load();
+    }
+}
