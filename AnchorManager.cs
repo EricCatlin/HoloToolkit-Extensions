9948bb9eb45bcbd68b445bc23b5d90d9637b562c
Eric Catlin <eric.catlin@indigoslate.com>
2017-07-03 11:14:59 -0700

    Anchor Manager and Anchor Mover are a slick way to move and update WorldAnchors

---
+++ AnchorManager.cs
@@ -0,0 +1,248 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR.WSA.Persistence;
+using UnityEngine.VR.WSA;
+using HoloToolkit.Unity.SpatialMapping;
+using System;
+using System.Linq;
+using UnityEngine.Events;
+using System.Collections;
+
+namespace HoloToolkit.Unity
+{
+
+    public class AnchorManager : MonoBehaviour
+    {
+        public string anchorName;
+
+
+        public AnchorMover AnchorMover;
+
+        public bool encode_rotation;
+        public bool encode_scale;
+
+        private WorldAnchorStore AnchorStore;
+        private string rotation_key = "_rotation";
+        private string scale_key = "_rotation";
+
+        void Awake()
+        {
+            Debug.Log("AnchorStore awake");
+
+            AnchorStore = null;
+            WorldAnchorStore.GetAsync(AnchorStoreReady);
+        }
+
+        public void AnchorStoreReady(WorldAnchorStore anchorStore)
+        {
+            Debug.Log("AnchorStore returned");
+            AnchorStore = anchorStore;
+            Load();
+        }
+
+        public void CenterOnMoverPlaceholder()
+        {
+            Overwrite(AnchorMover.Placeholder.transform.position);
+        }
+       
+
+
+        //Remove anchor but do not delete from anchor store
+        public void Remove()
+        {
+            Debug.Log("Remove Anchor on " + gameObject.name);
+
+            DestroyImmediate(GetComponent<WorldAnchor>());
+        }
+
+        //Remove anchor but do not delete from anchor store
+        public void Delete()
+        {
+            Debug.Log("Request Delete Anchor " + anchorName);
+            if (AnchorStore == null) { Debug.Log("AnchorStore is Null"); return; }
+
+            var ids = AnchorStore.GetAllIds();
+
+            for (int i = 0; i < ids.Length; i++)
+            {
+                var anchorTitle = ids[i];
+                Debug.Log(anchorTitle);
+
+                if (anchorTitle == anchorName)
+                {
+                    Debug.Log("Deleting");
+                    AnchorStore.Delete(anchorTitle);
+                }
+            }
+        }
+
+        //Upsert should create/update the anchor of name anchorName in the world anchor store to the location of the obj
+        //Upsert should create/update the 1 anchor attached to obj
+        //
+        internal void Load()
+        {
+            Debug.Log("Request Load Anchor " + anchorName + " : AnchorStore Loaded: " + (AnchorStore != null));
+
+            if (AnchorStore == null) { Debug.Log("AnchorStore is Null"); return; }
+
+            var ids = AnchorStore.GetAllIds();
+            for (int i = 0; i < ids.Length; i++)
+            {
+                var anchorTitle = ids[i];
+                if (anchorTitle == anchorName)
+                {
+                    AnchorStore.Load(anchorTitle, gameObject);
+                }
+            }
+
+            if (encode_rotation)
+            {
+                var rotation_transform = new GameObject();
+                for (int i = 0; i < ids.Length; i++)
+                {
+                    var anchorTitle = ids[i];
+                    if (anchorTitle == anchorName + rotation_key)
+                    {
+                        AnchorStore.Load(anchorTitle, rotation_transform);
+                        transform.LookAt(rotation_transform.transform.position);
+                    }
+                }
+            }
+
+            if (encode_scale)
+            {
+                var scale_transform = new GameObject();
+                for (int i = 0; i < ids.Length; i++)
+                {
+                    var anchorTitle = ids[i];
+                    if (anchorTitle == anchorName + scale_key)
+                    {
+                        AnchorStore.Load(anchorTitle, scale_transform);
+                    }
+                }
+            }
+        }
+       
+
+        //Upsert should create/update the anchor of name anchorName in the world anchor store to the location of the obj
+        //Upsert should create/update the 1 anchor attached to obj
+        //
+        internal void Create()
+        {
+
+            Debug.Log("Request Create Anchor " + anchorName + " : AnchorStore Loaded: " + (AnchorStore != null));
+
+            if (AnchorStore == null) { Debug.Log("AnchorStore is Null"); return; }
+            Debug.Log("Creating Anchor " + anchorName);
+            var worldAnchor = gameObject.AddComponent<WorldAnchor>();
+
+            if (worldAnchor.isLocated)
+            {
+                Debug.Log("Saving persisted position immediately");
+                bool saved = AnchorStore.Save(anchorName, worldAnchor);
+                Debug.Log("saved: " + saved);
+            }
+            else
+            {
+                Debug.Log("Setting a listener for main anchor");
+                worldAnchor.OnTrackingChanged += AttachingAnchor_OnTrackingChanged;
+            }
+
+            if (encode_rotation)
+            {
+                var rotation_transform = new GameObject();
+                rotation_transform.transform.position = transform.position + transform.forward;
+                var rotation_anchor = rotation_transform.AddComponent<WorldAnchor>();
+
+                if (rotation_anchor.isLocated)
+                {
+                    Debug.Log("Saving persisted position immediately");
+                    bool saved = AnchorStore.Save(anchorName + rotation_key, rotation_anchor);
+                    Debug.Log("saved rotation anchor: " + saved);
+                }
+                else
+                {
+                    Debug.Log("Setting a listener for rotation anchor");
+                    rotation_anchor.OnTrackingChanged += AttachingRotationAnchor_OnTrackingChanged;
+                }
+            }
+
+            if (encode_scale)
+            {
+                var scale_transform = new GameObject();
+                scale_transform.transform.position = transform.position + (transform.forward * transform.localScale.z) + (transform.right * transform.localScale.x) + (transform.up * transform.localScale.y);
+                var scale_anchor = scale_transform.AddComponent<WorldAnchor>();
+
+                if (scale_anchor.isLocated)
+                {
+                    Debug.Log("Saving persisted position immediately");
+                    bool saved = AnchorStore.Save(anchorName + scale_key, scale_anchor);
+                    Debug.Log("saved scale anchor: " + saved);
+                }
+                else
+                {
+                    Debug.Log("Setting a listener for scale anchor");
+                    scale_anchor.OnTrackingChanged += AttachingScaleAnchor_OnTrackingChanged;
+                }
+            }
+        }
+
+        public void AttachingAnchor_OnTrackingChanged(WorldAnchor self, bool located)
+        {
+            Debug.Log("AttachingAnchor_OnTrackingChanged");
+            if (located)
+            {
+                Debug.Log("Saving persisted position in callback");
+                bool saved = AnchorStore.Save(anchorName, self);
+                Debug.Log("saved: " + saved);
+                self.OnTrackingChanged -= AttachingAnchor_OnTrackingChanged;
+            }
+        }
+
+        public void AttachingRotationAnchor_OnTrackingChanged(WorldAnchor self, bool located)
+        {
+            Debug.Log("AttachingRotationAnchor_OnTrackingChanged");
+            if (located)
+            {
+                Debug.Log("Saving persisted position in callback");
+                bool saved = AnchorStore.Save(anchorName + rotation_key, self);
+                Debug.Log("saved: " + saved);
+                self.OnTrackingChanged -= AttachingRotationAnchor_OnTrackingChanged;
+            }
+        }
+
+        public void AttachingScaleAnchor_OnTrackingChanged(WorldAnchor self, bool located)
+        {
+            Debug.Log("AttachingScaleAnchor_OnTrackingChanged");
+            if (located)
+            {
+                Debug.Log("Saving persisted position in callback");
+                bool saved = AnchorStore.Save(anchorName + scale_key, self);
+                Debug.Log("saved: " + saved);
+                self.OnTrackingChanged -= AttachingScaleAnchor_OnTrackingChanged;
+            }
+        }
+
+        //Upsert should create/update the anchor of name anchorName in the world anchor store to the location of the obj
+        //Upsert should create/update the 1 anchor attached to obj
+        //
+        internal void Overwrite(Vector3 desiredLocation)
+        {
+
+            Debug.Log("Request Overwrite Anchor " + anchorName + " : AnchorStore Loaded: " + (AnchorStore != null));
+
+            if (AnchorStore == null) { Debug.Log("AnchorStore is Null"); return; }
+            Delete();
+            Remove();
+            gameObject.transform.position = desiredLocation;
+            if(encode_rotation && AnchorMover)
+            {
+                transform.rotation = AnchorMover.Placeholder.transform.rotation;
+            }
+            Create();
+
+            
+        }
+    }
+}
---
+++ AnchorMover.cs
@@ -0,0 +1,83 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+using UnityEngine.VR.WSA.Persistence;
+using UnityEngine.VR.WSA;
+using System.Collections;
+using UnityEngine.Events;
+
+namespace HoloToolkit.Unity.SpatialMapping
+{
+    //The city placer acts as the main controller for all placement needs of the city.
+    //It should coordinate control between itself and the target-tracker, as well as read/write anchor data for the rig.
+    public class AnchorMover : MonoBehaviour, IInputClickHandler
+    {
+        //Outwardly accessible state
+        public bool IsBeingPlaced;
+        public UnityEvent OnPositionSet;
+
+        //Mover internals
+        public GameObject Placeholder;
+
+
+        public Vector3 FinalOrientation;
+        public float PlacementFlexibility;
+
+        public float MinDistanceFromTarget;
+
+        //Privates
+        protected SpatialMappingManager spatialMappingManager;
+        private Vector3 default_placeholder_scale;
+
+        //Set Private internals
+        protected virtual void Start()
+        {
+            spatialMappingManager = SpatialMappingManager.Instance;
+            default_placeholder_scale = Placeholder.transform.localScale;
+        }
+
+        //Smoothly reposition the thing
+        protected virtual void FixedUpdate()
+        {
+            //Get gaze targeting info
+            Vector3 headPosition = Camera.main.transform.position;
+            Vector3 gazeDirection = Camera.main.transform.forward;
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(headPosition, gazeDirection, out hitInfo, 30.0f, spatialMappingManager.LayerMask))
+            {
+                var distance_from_target = Vector3.Distance(transform.position, hitInfo.point);
+                var degrees_away_from_final_orientation = Quaternion.Angle(Quaternion.Euler(hitInfo.normal), Quaternion.Euler(FinalOrientation));
+
+                //If gaze is on a wall suitable surface and far enough from the target object, move the placeholder there
+                if (distance_from_target > MinDistanceFromTarget && degrees_away_from_final_orientation < PlacementFlexibility)
+                {
+                    IsBeingPlaced = true;
+                    Placeholder.SetActive(true);
+
+                    Placeholder.transform.localScale = default_placeholder_scale * (1 - (Vector3.Distance(Placeholder.transform.position, hitInfo.point)));
+                    Placeholder.transform.position = Vector3.Lerp(Placeholder.transform.position, hitInfo.point, 0.05f);
+                }
+                else
+                {
+                    Placeholder.transform.position = hitInfo.point;
+                    IsBeingPlaced = false;
+                    Placeholder.SetActive(false);
+                }
+            }
+        }
+
+        public virtual void OnInputClicked(InputClickedEventData eventData)
+        {
+            OnInputClicked();
+        }
+        public virtual void OnInputClicked()
+        {
+            if (!IsBeingPlaced) return;
+            OnPositionSet.Invoke();
+        }
+    }
+}
